8b2cd66cb03e4f3eba833a094b0d30c3bbb2c09a
AFaure <aldric91@hotmail.fr>
2019-03-23 04:50:29 +0100

    Ajout Scripts

---
+++ Assets/Scripts/PartieManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+    Classe permettant les interactions du joueur durant la partie
+    - Demande d'indice
+    - Proposition d'indice
+    - Gestion UI
+    - Affichage chatbot
+    etc
+ */
+public class PartieManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Scenario.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/**
+    Objet conteneur des informations du scénario
+*/
+[System.Serializable]
+public class Scenario
+{
+
+    // TODO : ajouter informations
+
+}
