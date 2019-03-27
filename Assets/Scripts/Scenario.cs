3620f8edc3838f021f4d7ddc774485a63f89cb45
NaClMalzeur <eman.mertz@gmail.com>
2019-03-27 09:20:34 +0100

    Ajout du passage de l'arbre de scénario json en objet c#
    Test non fonctionnel, voir avec aldrick

--- Assets/Scripts/Scenario.cs
+++ Assets/Scripts/Scenario.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
+using System;
+using System.IO;
 
 /**
     Objet conteneur des informations du scénario
@@ -7,6 +9,41 @@
 public class Scenario
 {
 
-    // TODO : ajouter informations
+    private Enigme[] enigmes;
+    private Indice[] indices;
+
+    public Scenario(Enigme[] enigmes, Indice[] indices)
+    {
+        this.enigmes = enigmes;
+        this.indices = indices;
+    }
+
+    public static Scenario CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<Scenario>(jsonString);
+    }
+
+    public static Scenario CreateFromFile(string file)
+    {
+        string jsonString = "";
+        try 
+        {
+            using (StreamReader sr = new StreamReader(file)) 
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null) 
+                {
+                    jsonString += "\n" + line;
+                    Debug.Log(line);
+                }
+            }
+        }
+        catch (Exception e) 
+        {
+            Debug.Log("The file could not be read:");
+             Debug.Log(e.Message);
+        }
+        return CreateFromJSON(jsonString);
+    }
 
 }
